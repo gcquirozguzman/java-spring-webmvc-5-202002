063d97ef902351204e38f7a2a5693aeb1f8d38ee
Shester Developer <gcquirozguzman@gmail.com>
2020-02-14 02:54:15 -0500

    Se agrega tabla y registros para prueba.

--- src/main/webapp/resources/sql/create-table.sql
+++ src/main/webapp/resources/sql/create-table.sql
@@ -1,3 +1,5 @@
+--Tabla con datos de empleado
+
 CREATE TABLE `Employee` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(20) NOT NULL,
@@ -6,4 +8,14 @@ CREATE TABLE `Employee` (
   `salario` DECIMAL(100,2) NOT NULL,
   `sexo` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
+);
+
+--Tabla detalle de estudios de empleado
+
+CREATE TABLE `Studies` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id_employee` int(10) NOT NULL,
+  `studie` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  foreign key (id_employee) references Employee(id)
 );
